ab60da016c506c7fb0d9cdae7333640a9e2a175c
Chino <akahemenokwu@gmail.com>
2022-09-26 06:53:10 +0100

    testing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    listlength = len(my_list) - 1
-    if (idx < 0 or idx > listlength):
-        return (my_list)
+    if idx < 0:
+        return None
+    elif idx > (len(my_list) - 1):
+        return None
+
     else:
-        return (my_list[idx])
+        return my_list[idx]
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
-    return ("".join(listofchars))
+    copy = [char for char in my_string if char != 'c' and char != 'C']
+    return ("".join(copy))
